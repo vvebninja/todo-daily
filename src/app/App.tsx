9291ef6ace6eea1f96f8bd71a26dd3514de10ace
vvebninja <aleksandru.morar@gmail.com>
2026-01-23 15:34:44 +0200

    chore: add .env.example and  configs

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router";
+import { CONFIG } from "shared/model/config";
 
 export function App() {
   return (
     <div>
+      <div>{CONFIG.API_BASE_URL}</div>
       <Outlet />
     </div>
   );
