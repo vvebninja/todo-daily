738aa0cfe069e1c1d26e210aa31ff9e9f16a6b01
vvebninja <aleksandru.morar@gmail.com>
2026-02-21 09:07:12 +0200

    refactor(auth): change title font and form positon

--- src/features/auth/ui/auth-layout.tsx
+++ src/features/auth/ui/auth-layout.tsx
@@ -4,11 +4,11 @@ import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/shared/u
 
 export function AuthLayout(props: { title: ReactNode; form: ReactNode; footer: ReactNode }) {
   return (
-    <main className="flex grow flex-col items-center px-4 pt-40">
+    <main className="flex grow flex-col items-center px-4 pt-25">
       <Card className="w-full max-w-125 gap-8 rounded-[6px] p-10 pb-24">
         <CardHeader className="gap-10">
           <AppLogo variant="secondary" />
-          <CardTitle className="text-2xl font-bold">{props.title}</CardTitle>
+          <CardTitle className="font-secondary text-2xl font-bold">{props.title}</CardTitle>
         </CardHeader>
         <CardContent className="">{props.form}</CardContent>
         <CardFooter className="flex items-center justify-center">
