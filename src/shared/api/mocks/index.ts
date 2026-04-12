c7e88d1c6e271278ce3666bde4f2d6784e1062be
vvebninja <olexandr.morar@gmail.com>
2026-04-12 18:04:57 +0300

    fix(api/mocks): disable production check to enable mocking in all environments

--- src/shared/api/mocks/index.ts
+++ src/shared/api/mocks/index.ts
@@ -1,7 +1,7 @@
 export async function enableMocking() {
-  if (import.meta.env.PROD) {
-    return
-  }
+  // if (import.meta.env.PROD) {
+  //   return
+  // }
 
   const { worker } = await import('@/shared/api/mocks/browser')
   return worker.start()
