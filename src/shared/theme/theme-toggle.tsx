04c3e4d7f3db82d71be3a27ad679ed252d48ac99
vvebninja <olexandr.morar@gmail.com>
2026-04-24 04:57:15 +0300

    style: update theme toggle background color for dark mode

--- src/shared/theme/theme-toggle.tsx
+++ src/shared/theme/theme-toggle.tsx
@@ -10,7 +10,7 @@ export function ThemeToggle() {
       variant="outline"
       size="icon"
       onClick={toggleTheme}
-      className="rounded-full"
+      className="dark:bg-card rounded-full"
     >
       <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
       <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
