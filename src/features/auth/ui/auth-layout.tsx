143a7b711837b2fdb5245a4e5fe872ae759afae8
vvebninja <olexandr.morar@gmail.com>
2026-04-24 06:35:51 +0300

    feat: add background blur effect and shadow to AuthLayout card and update AppLogo styling

--- src/features/auth/ui/auth-layout.tsx
+++ src/features/auth/ui/auth-layout.tsx
@@ -18,9 +18,10 @@ type AuthLayoutProps = Readonly<{
 export function AuthLayout(props: AuthLayoutProps) {
   return (
     <main className="flex grow flex-col items-center px-4 pt-50">
-      <Card className="w-full max-w-125 gap-8 rounded-[6px] p-10">
+      <div className="bg-primary absolute -z-1 h-50 w-50 rounded-full blur-[120px]" />
+      <Card className="w-full max-w-125 gap-8 rounded-[6px] p-10 shadow-2xl">
         <CardHeader className="mb-10 justify-center gap-10">
-          <AppLogo variant="secondary" />
+          <AppLogo variant="secondary" className="bg-transparent" />
           {props.title && (
             <CardTitle className="font-secondary text-2xl font-bold">
               {props.title}
