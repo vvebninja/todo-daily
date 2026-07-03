f68e00c9514f6b07645b2f967720be8a43610258
vvebninja <olexandr.morar@gmail.com>
2026-07-03 21:49:40 +0300

    refactor(todo-create-form): WIP add  bullet, checkbox, and numeric items for a todo [skip ci]

--- src/features/todo/ui/create-form.tsx
+++ src/features/todo/ui/create-form.tsx
@@ -1,9 +1,24 @@
-import type { SubmitEvent } from 'react'
-import { PlusIcon } from 'lucide-react'
-import { toast } from 'sonner'
+import type { ChangeEvent, KeyboardEvent, SubmitEvent } from 'react'
+import {
+  ListCheckIcon,
+  ListIcon,
+  ListOrderedIcon,
+  PlusIcon,
+} from 'lucide-react'
+import { useEffect, useRef, useState } from 'react'
 import { useCreateTodo } from '@/features/todo/model/use-create-todo'
+import { cn } from '@/shared/lib/css'
 import { Button } from '@/shared/ui/kit/button.tsx'
+import { Field } from '@/shared/ui/kit/field'
 import { Input } from '@/shared/ui/kit/input'
+
+interface TaskItem { id: string, text: string }
+
+interface Task {
+  id: string
+  title?: string
+  items: TaskItem[]
+}
 
 interface CreateFormProps {
   className?: string
@@ -11,44 +26,168 @@ interface CreateFormProps {
 
 export function CreateTodoForm({ className }: CreateFormProps) {
   const createTodo = useCreateTodo()
+  const [isExpanded, setIsExpanded] = useState(false)
+  const formRef = useRef<HTMLFormElement>(null)
+  const taskTextRef = useRef<HTMLInputElement>(null)
+  const [isFocused, setIsFocused] = useState(false)
+  const [task, setTask] = useState<Task>({ id: '', title: '', items: [] })
 
   const handleSubmit = (e: SubmitEvent<HTMLFormElement>) => {
     e.preventDefault()
 
-    const form = e.target
-    const formData = new FormData(e.currentTarget)
-    const title = String(formData.get('title')).trim()
-
-    if (!title)
-      return
-
-    toast.promise(createTodo.mutateAsync({ title }), {
-      loading: 'Creating todo... ⏳',
-      success: () => 'Todo created! 🎉',
-      error: err => err?.message || 'Error creating todo ❌',
-    })
-
-    form.reset()
+    // toast.promise(createTodo.mutateAsync({ title }), {
+    //   loading: 'Creating todo... ⏳',
+    //   success: () => 'Todo created! 🎉',
+    //   error: err => err?.message || 'Error creating todo ❌',
+    // })
   }
 
+  const handleTaskTitleKeydown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.code === 'Enter') {
+      taskTextRef.current?.focus()
+    }
+
+    if (e.ctrlKey && e.code === 'Enter') {
+      alert('submitted')
+    }
+  }
+
+  const handleTaskTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setTask(prev => ({ ...prev, title: e.target.value }))
+  }
+
+  const handleEditTaskChange = (e: ChangeEvent<HTMLInputElement>, id: TaskItem['id']) =>
+    setTask(prev => ({
+      ...prev,
+      items: prev.items.map(innerItem =>
+        innerItem.id === id
+          ? { ...innerItem, text: e.target.value }
+          : innerItem,
+      ),
+    }))
+
+  const handleTaskTextKeydown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.code === 'Enter') {
+      const text = e.currentTarget.value
+
+      setTask(prev => ({
+        ...prev,
+        items: [...prev.items, { id: Date.now().toString(), text }],
+      }))
+
+      e.currentTarget.value = ''
+    }
+  }
+
+  useEffect(() => {
+    if (!formRef)
+      return
+
+    const handleClickOutside = (e) => {
+      if (!formRef.current?.contains(e.target as Node)) {
+        setIsExpanded(false)
+      }
+    }
+
+    document.addEventListener('click', handleClickOutside)
+
+    return () => {
+      removeEventListener('click', handleClickOutside)
+    }
+  }, [])
+
   return (
-    <form onSubmit={handleSubmit} className={className}>
-      <div className="relative">
+    <form
+      ref={formRef}
+      onSubmit={handleSubmit}
+      className={cn('p-2 bg-card rounded-lg', className)}
+    >
+      {/* Input Title */}
+      <Field orientation="horizontal">
         <Input
           name="title"
-          placeholder="Task..."
-          className="dark:focus-visible:shadow-primary/20 focus-visible:border-foreground placeholder:text-muted-foreground/80 border-b-primary/20 h-10 pr-10 transition-all duration-300 focus-visible:shadow-md focus-visible:ring-0 md:h-11 md:rounded-lg lg:h-12"
+          placeholder={isExpanded ? 'Title' : 'Take a note...'}
+          onFocus={() => setIsExpanded(true)}
+          onChange={handleTaskTitleChange}
+          onKeyDown={handleTaskTitleKeydown}
+          className="h-11 pl-4 dark:bg-transparent border-0 border-b border-b-transparent rounded-none focus-visible:ring-0 dark:focus-visible:border-b-primary/30 focus-visible:placeholder-primary"
         />
         <Button
-          type="submit"
+          type="button"
           size="icon"
           variant="ghost"
           disabled={createTodo.isPending}
-          className="hover:text-primary text-muted-foreground/80 absolute inset-y-0 right-1 my-auto transition-colors duration-300"
+          className="hover:text-primary transition-colors duration-300"
         >
-          <PlusIcon className="size-5" />
+          <PlusIcon className="size-6" />
         </Button>
-      </div>
+      </Field>
+
+      {isExpanded && (
+        <div>
+          {/* Input Task Text  */}
+          <Input
+            ref={taskTextRef}
+            type="text"
+            name="taskText"
+            placeholder="Take a note..."
+            onKeyDown={handleTaskTextKeydown}
+            className="min-h-12 pl-4 dark:bg-transparent rounded-none border-0 focus-visible:ring-0 focus-visible:placeholder-primary"
+          />
+
+          <div className="pl-8">
+            <ul className="list-disc">
+              {task.items.map(item => (
+                <li key={item.id}>
+                  {isFocused
+                    ? (
+                      <Input
+                        type="text"
+                        value={item.text}
+                        onChange={e => handleEditTaskChange(e, task.id)}
+                        // Check what setIsFocused does
+                        // Why on focus edit input doesn't appear
+                        onFocus={() => setIsFocused(true)}
+                        className="dark:bg-transparent border-none focus-visible:ring-0 focus-visible:text-primary"
+                      />
+                    )
+                    : (
+                      <span>{item.text}</span>
+                    )}
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <ul className="flex items-center justify-end">
+            <li>
+              <Button
+                variant="ghost"
+                className="text-muted-foreground/80 rounded-r-none"
+              >
+                <ListOrderedIcon className="size-5" />
+              </Button>
+            </li>
+            <li>
+              <Button
+                variant="ghost"
+                className="text-muted-foreground/80 rounded-none"
+              >
+                <ListIcon className="size-5" />
+              </Button>
+            </li>
+            <li>
+              <Button
+                variant="ghost"
+                className="text-muted-foreground/80 rounded-l-none"
+              >
+                <ListCheckIcon className="size-5" />
+              </Button>
+            </li>
+          </ul>
+        </div>
+      )}
+
     </form>
   )
 }
